9d28279098d844d240e0395133a2c5e168a5881d
Rishabh Dixit <rishabhrajat23sep@gmail.com>
2024-06-02 18:57:09 +0530

    Jwt files added

--- JwtExample/src/main/java/com/jwt/example/JwtExample/controller/HomeController.java
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/controller/HomeController.java
@@ -3,11 +3,13 @@ package com.jwt.example.JwtExample.controller;
 
 import com.jwt.example.JwtExample.models.User;
 import com.jwt.example.JwtExample.service.UserService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -19,9 +21,12 @@ public class HomeController {
 
     @GetMapping("/user")
     public List<User> getUser(){
-
         System.out.println("getting users");
-
         return this.userService.getUsers();
+    }
+
+    @GetMapping("/current-user")
+    public String getLoggedInUser(Principal principal){
+        return principal.getName();
     }
 }
