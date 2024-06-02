3162bd9ffe1132993201f45c27058337eedd4e13
Rishabh Dixit <rishabhrajat23sep@gmail.com>
2024-06-02 15:28:11 +0530

    config file added

---
+++ JwtExample/src/main/java/com/jwt/example/JwtExample/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.jwt.example.JwtExample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+       UserDetails user = User.builder().username("gravity").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
